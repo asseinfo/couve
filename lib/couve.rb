daf53a4c6f093fe8010ce0d706c2b309fcb6b8cc
Cezinha <cesar@asseinfo.com.br>
2023-07-19 09:10:45 -0300

    Initial commit

---
+++ lib/couve.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "couve/version"
+
+module Couve
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/couve/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Couve
+  VERSION = "0.1.0"
+end
